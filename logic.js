aa460789be4e45a8c94555a3d48090e6711673b3
hydro510 <ricardobc07@outlook.com>
2026-07-15 01:07:18 -0500

    added click and drag logic

--- logic.js
+++ logic.js
@@ -1,9 +1,13 @@
 let squares = '';
-
+let isDrawing = false;
 let infoObject = document.querySelector(".dimension");
+
+document.addEventListener("mousedown",()=> isDrawing = true);
+document.addEventListener("mouseup",()=> isDrawing = false);
 infoObject.addEventListener("keydown",(event)=>{
     if(event.key==="Enter"){
        let dimension = Number(infoObject.value);
+       
 
        let main = document.querySelector(".main-container");
        main.style.setProperty(`--grid-size`,`${dimension}`);
@@ -15,6 +19,38 @@ infoObject.addEventListener("keydown",(event)=>{
 
         document.querySelector(".main-container").innerHTML = squares;
         squares = '';
+
+        getsClicked();
     }
 });
+
+
+function getsClicked(){
+     document.querySelectorAll(".square").forEach((square)=>{
+            let light = 100;
+            function draw(){
+                    square.classList.add("clicked");
+                    if(light>0){
+                        light-=10;
+                        square.style.setProperty('--light',`${light}%`)
+                    }
+            }
+            square.addEventListener("mousedown",(e)=>{
+                e.preventDefault();
+                draw();
+            });
+
+            square.addEventListener("mouseenter",()=>{
+                if(isDrawing){
+                    draw();
+                }
+            });
+
+           
+
+        });
+}
+
+
+
 
